d3bf0d2597b387bb8fed05577eb5385ec68167d7
TjandraD <tjandra.darmo@ajaib.co.id>
2023-03-05 21:19:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# File Processor (DSP Test)
+
+WIP
---
+++ src/main/java/com/tjandra/dspfileprocessor/DspFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.tjandra.dspfileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspFileProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DspFileProcessorApplication.class, args);
+    }
+
+}
